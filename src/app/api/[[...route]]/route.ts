c0dc6a5badd5327f7c460d30c091d6f87b991bca
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-14 06:48:39 +0530

    best practices

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,28 +1,32 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import test from "node:test";
-import { z } from "zod";
-import { zValidator } from "@hono/zod-validator";
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+// import test from "node:test";
+// import { z } from "zod";
+// import { zValidator } from "@hono/zod-validator";
+// import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import authors from "./authors";
+import books from "./books";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
+app.route("/authors", authors);
+app.route("/books", books);
 
-app.use("*", clerkMiddleware()).get("/home", clerkMiddleware(), (c) => {
-  const auth = getAuth(c);
+// app.use("*", clerkMiddleware()).get("/home", clerkMiddleware(), (c) => {
+//   const auth = getAuth(c);
 
-  if (!auth?.userId) {
-    return c.json({
-      message: "You are not logged in.",
-    });
-  }
+//   if (!auth?.userId) {
+//     return c.json({
+//       message: "You are not logged in.",
+//     });
+//   }
 
-  return c.json({
-    message: "You are logged in!",
-    userId: auth.userId,
-  });
-});
+//   return c.json({
+//     message: "You are logged in!",
+//     userId: auth.userId,
+//   });
+// });
 // app
 //   .get("/hello", (c) => {
 //     return c.json({
