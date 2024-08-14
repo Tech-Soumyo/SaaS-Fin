794f8d6e7aec2d8c6c5711c0ecd6669913141aea
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-14 06:03:34 +0530

    how to Clerk middleware  in hono

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -9,54 +9,54 @@ export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-// app.use("*", clerkMiddleware()).get("/home", clerkMiddleware(), (c) => {
-//   const auth = getAuth(c);
+app.use("*", clerkMiddleware()).get("/home", clerkMiddleware(), (c) => {
+  const auth = getAuth(c);
 
-//   if (!auth?.userId) {
+  if (!auth?.userId) {
+    return c.json({
+      message: "You are not logged in.",
+    });
+  }
+
+  return c.json({
+    message: "You are logged in!",
+    userId: auth.userId,
+  });
+});
+// app
+//   .get("/hello", (c) => {
 //     return c.json({
-//       message: "You are not logged in.",
+//       message: "Hello Next.js!",
 //     });
-//   }
-
-//   return c.json({
-//     message: "You are logged in!",
-//     userId: auth.userId,
-//   });
-// });
-app
-  .get("/hello", (c) => {
-    return c.json({
-      message: "Hello Next.js!",
-    });
-  })
-  .get("/hello/:id", (c) => {
-    return c.json({
-      message: "It is my id",
-      test: test,
-    });
-  })
-  .post(
-    "/create/:postId",
-    zValidator(
-      "json",
-      z.object({
-        name: z.string(),
-        userId: z.number(),
-      })
-    ),
-    zValidator(
-      "param",
-      z.object({
-        postId: z.number(),
-      })
-    ),
-    (c) => {
-      const { name, userId } = c.req.valid("json");
-      const { postId } = c.req.valid("param");
-      // ... use your validated data
-      return c.json({});
-    }
-  );
+//   })
+//   .get("/hello/:id", (c) => {
+//     return c.json({
+//       message: "It is my id",
+//       test: test,
+//     });
+//   })
+//   .post(
+//     "/create/:postId",
+//     zValidator(
+//       "json",
+//       z.object({
+//         name: z.string(),
+//         userId: z.number(),
+//       })
+//     ),
+//     zValidator(
+//       "param",
+//       z.object({
+//         postId: z.number(),
+//       })
+//     ),
+//     (c) => {
+//       const { name, userId } = c.req.valid("json");
+//       const { postId } = c.req.valid("param");
+//       // ... use your validated data
+//       return c.json({});
+//     }
+//   );
 
 export const GET = handle(app);
 export const POST = handle(app);
