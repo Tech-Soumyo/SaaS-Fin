416f1dc679f2c31080c2631dd665659d3daf0f7e
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-15 10:07:59 +0530

    set page for dashboard

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,5 +2,9 @@ import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 export default function Home() {
-  return <UserButton afterSwitchSessionUrl="/" />;
+  return (
+    <>
+      <div>Dashboard</div>
+    </>
+  );
 }
