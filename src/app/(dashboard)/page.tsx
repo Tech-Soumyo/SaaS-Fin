4b519158d9d0b83f1e30587b80da1ab22c0d8293
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-20 20:54:59 +0530

    try to implement useGetAccount hook

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,9 +1,18 @@
-import { UserButton } from "@clerk/nextjs";
+// import { useGetAccounts } from "@/features-hooks/accounts/api/use-get-accounts.hook";
 import React from "react";
 
 export default function Home() {
+  // const { data: accounts, isLoading } = useGetAccounts();
+  // if (isLoading) {
+  //   return <div>Loading...</div>;
+  // }
   return (
     <>
+      {/* <div>
+        {accounts?.map((account) => (
+          <div key={account.id}>{account.name}</div>
+        ))}
+      </div> */}
       <div>Dashboard</div>
     </>
   );
