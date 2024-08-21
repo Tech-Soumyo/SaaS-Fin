977590cf0e1e31a05288bfdcd5c59653de059337
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-22 01:23:29 +0530

    Add Account UI

---
+++ src/components/custom/new-account-sheet.tsx
@@ -0,0 +1,51 @@
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { AccountForm } from "./account-form";
+import { insertAccountsSchema } from "@/db/schema";
+import { z } from "zod";
+import { useCreateAccount } from "@/features/accounts/api/use-create-account";
+
+const formSchema = insertAccountsSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
+export const NewAccountSheet = () => {
+  const { isOpen, onClose } = useNewAccount();
+
+  const mutation = useCreateAccount();
+
+  const onSubmit = (values: FormValues) => {
+    mutation.mutate(values);
+  };
+
+  return (
+    <>
+      <Sheet open={isOpen} onOpenChange={onClose}>
+        {/* <SheetTrigger>Open</SheetTrigger> */}
+        <SheetContent className="space-y-4">
+          <SheetHeader>
+            <SheetTitle>New Account</SheetTitle>
+            <SheetDescription>
+              Create New Account to Track your Transactions
+            </SheetDescription>
+          </SheetHeader>
+          <AccountForm
+            onSubmit={onSubmit}
+            disabled={mutation.isPending}
+            defaultValues={{
+              name: "",
+            }}
+          />
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+};
