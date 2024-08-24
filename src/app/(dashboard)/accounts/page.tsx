72f5b010fe83dd6befdccbe1bb29afe263522e2a
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-24 14:23:57 +0530

    Add features like Add accounts and delete Accounts

--- src/app/(dashboard)/accounts/page.tsx
+++ src/app/(dashboard)/accounts/page.tsx
@@ -9,10 +9,13 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { Plus } from "lucide-react";
-import { Payment, columns } from "@/app/(dashboard)/accounts/columns";
+import { useNewAccount } from "../../../hooks/features/accounts/use-new-account";
+import { Loader2, Plus } from "lucide-react";
+import { columns } from "@/app/(dashboard)/accounts/columns";
 import { DataTable } from "@/components/ui/data-table";
+import { useGetAccounts } from "@/hooks/features/accounts/use-get-accounts.hook";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useBulkDeleteAccounts } from "@/hooks/features/accounts/use-bulk-delete-accounts";
 
 // export default function AccountPage() {
 //   return (
@@ -22,23 +25,47 @@ import { DataTable } from "@/components/ui/data-table";
 //   );
 // }
 
-const data: Payment[] = [
-  {
-    id: "728ed52f",
-    amount: 100,
-    status: "pending",
-    email: "m@example.com",
-  },
-  {
-    id: "728ed54s",
-    amount: 50,
-    status: "success",
-    email: "a@example.com",
-  },
-];
+// const data: Payment[] = [
+//   {
+//     id: "728ed52f",
+//     amount: 100,
+//     status: "pending",
+//     email: "m@example.com",
+//   },
+//   {
+//     id: "728ed54s",
+//     amount: 50,
+//     status: "success",
+//     email: "a@example.com",
+//   },
+// ];
 
 const AccountPage = () => {
   const newAccount = useNewAccount();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || [];
+
+  const deleteAccounts = useBulkDeleteAccounts();
+  const isDisabled = accountsQuery.isLoading || deleteAccounts.isPending;
+
+  if (accountsQuery.isLoading) {
+    return (
+      <>
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+          <Card className="border-none drop-shadow-sm">
+            <CardHeader>
+              <Skeleton className="h-8 w-full" />
+            </CardHeader>
+            <CardContent>
+              <div className="h-[500px] w-full flex items-center justify-center">
+                <Loader2 className="size-6 text-slate-300 animate-sping" />
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </>
+    );
+  }
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -55,10 +82,13 @@ const AccountPage = () => {
         <CardContent>
           <DataTable
             columns={columns}
-            data={data}
+            data={accounts}
             filterKey="email"
-            onDelete={() => {}}
-            disabled={false}
+            onDelete={(row) => {
+              const ids = row.map((r) => r.original.id);
+              deleteAccounts.mutate({ ids });
+            }}
+            disabled={isDisabled}
           />
         </CardContent>
       </Card>
