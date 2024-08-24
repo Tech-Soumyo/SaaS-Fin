806dc2fb55954eedc8069b24b6232dc6ed299252
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-24 14:21:10 +0530

    changing the UI of the data rows

--- src/app/(dashboard)/accounts/columns.tsx
+++ src/app/(dashboard)/accounts/columns.tsx
@@ -1,5 +1,6 @@
 "use client";
-
+import { InferResponseType } from "hono";
+import { client } from "@/lib/hono-RPC";
 import { ColumnDef } from "@tanstack/react-table";
 
 import { Button } from "@/components/ui/button";
@@ -8,14 +9,20 @@ import { Checkbox } from "@/components/ui/checkbox";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type Payment = {
-  id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
-  email: string;
-};
 
-export const columns: ColumnDef<Payment>[] = [
+// export type Payment = {
+//   id: string;
+//   amount: number;
+//   status: "pending" | "processing" | "success" | "failed";
+//   email: string;
+// };
+
+export type ResponseType = InferResponseType<
+  typeof client.api.accounts.$get,
+  200
+>["data"][0];
+
+export const columns: ColumnDef<ResponseType>[] = [
   {
     // Selection column
     id: "select",
@@ -40,26 +47,18 @@ export const columns: ColumnDef<Payment>[] = [
     enableHiding: false,
   },
   {
-    accessorKey: "status",
-    header: "Status",
-  },
-  {
     // Email column with sorting functionality
-    accessorKey: "email",
+    accessorKey: "name",
     header: ({ column }) => {
       return (
         <Button
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-          Email
+          Name
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       );
     },
-  },
-  {
-    accessorKey: "amount",
-    header: "Amount",
   },
 ];
