9dbf160eb8e7566651d5a2249defd417f4d1bce3
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-25 16:40:40 +0530

    delete extra code

--- src/hooks/features/accounts/use-bulk-delete-accounts.ts
+++ src/hooks/features/accounts/use-bulk-delete-accounts.ts
@@ -1,38 +1,3 @@
-// import { toast } from "sonner";
-// import { InferRequestType, InferResponseType } from "hono";
-// import { useMutation, useQueryClient } from "@tanstack/react-query";
-
-// import { client } from "@/lib/hono-RPC";
-
-// type ResponseType = InferResponseType<
-//   (typeof client.api.accounts)["bulk-delete"]["$post"]
-// >;
-// type RequestType = InferRequestType<
-//   (typeof client.api.accounts)["bulk-delete"]["$post"]
-// >["json"];
-
-// export const useBulkDeleteAccounts = () => {
-//   const queryClient = useQueryClient();
-
-//   const mutation = useMutation<ResponseType, Error, RequestType>({
-//     mutationFn: async (json) => {
-//       const response = await client.api.accounts["bulk-delete"]["$post"]({
-//         json,
-//       });
-//       return await response.json();
-//     },
-//     onSuccess: () => {
-//       toast.success("Account Deleted");
-//       queryClient.invalidateQueries({ queryKey: ["accounts"] });
-//       //   TODO: Also Validate Summary
-//     },
-//     onError: () => {
-//       toast.error("Failed to Delete account");
-//     },
-//   });
-
-//   return mutation;
-// };
 import { toast } from "sonner";
 import { InferRequestType, InferResponseType } from "hono";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
@@ -57,12 +22,12 @@ export const useBulkDeleteAccounts = () => {
       return await response.json();
     },
     onSuccess: () => {
-      toast.success("Account deleted");
+      toast.success("Account Deleted");
       queryClient.invalidateQueries({ queryKey: ["accounts"] });
-      // TODO: Also invalidate summary
+      //   TODO: Also Validate Summary
     },
     onError: () => {
-      toast.error("Failed to delete account");
+      toast.error("Failed to Delete account");
     },
   });
 
