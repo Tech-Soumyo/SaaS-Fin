a66720605bf4f1d16991dac575a58e85c0b3997a
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-26 13:49:39 +0530

    All the category hooks

---
+++ src/hooks/features/categories/use-bulk-delete-categories.ts
@@ -0,0 +1,35 @@
+import { toast } from "sonner";
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { client } from "@/lib/hono-RPC";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.categories)["bulk-delete"]["$post"]
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.categories)["bulk-delete"]["$post"]
+>["json"];
+
+export const useBulkDeleteCategories = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.categories["bulk-delete"]["$post"]({
+        json,
+      });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Category Deleted");
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+      //   TODO: Also Validate Summary
+    },
+    onError: () => {
+      toast.error("Failed to Delete Categories");
+    },
+  });
+
+  return mutation;
+};
