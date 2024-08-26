9082626a53d5b99e5d7fe680c71c28ed98b43369
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-26 12:54:41 +0530

    upgrade the drizle kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,21 @@
 import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
+import { parse } from "pg-connection-string"; // You can use this library to parse the connection string
 
 config({ path: ".env" });
 
+const dbConfig = parse(process.env.DATABASE_URL!);
+
 export default defineConfig({
   schema: "./src/db/schema.ts",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    host: dbConfig.host!,
+    port: dbConfig.port ? parseInt(dbConfig.port) : undefined,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.database!,
+    ssl: dbConfig.ssl as boolean | undefined, // Adjust this as needed
   },
   verbose: true,
   strict: true,
