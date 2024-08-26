4b59a2bcc0b6358a56a3cd92b917774dc704240c
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-26 13:05:34 +0530

    individually category form for editing and deletion

---
+++ src/components/custom/category-form.tsx
@@ -0,0 +1,99 @@
+import { insertCategoriesSchema } from "@/db/schema";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Trash } from "lucide-react";
+
+const formSchema = insertCategoriesSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
+
+type Props = {
+  id?: string;
+  defaultValues?: FormValues;
+  onSubmit: (values: FormValues) => void;
+  onDelete?: () => void;
+  disabled?: boolean;
+};
+
+export const CategoryForm = ({
+  id,
+  defaultValues,
+  onSubmit,
+  onDelete,
+  disabled,
+}: Props) => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: defaultValues,
+  });
+
+  const handleSubmit = (values: FormValues) => {
+    onSubmit(values);
+  };
+
+  const handleDelete = () => {
+    onDelete?.();
+  };
+
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="space-y-4 pt-4"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={disabled}
+                    placeholder="e.g. Food, Travel, etc."
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  This is your public Category display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="w-full" disabled={disabled}>
+            {id ? "Save changes" : "Create Category"}
+          </Button>
+          {!!id && (
+            <Button
+              type="button"
+              disabled={disabled}
+              onClick={handleDelete}
+              className="w-full"
+              variant="outline"
+            >
+              <Trash className="size-4 mr-2" />
+              Delete Category
+            </Button>
+          )}
+        </form>
+      </Form>
+    </>
+  );
+};
