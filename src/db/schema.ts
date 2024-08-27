72e8fd4faa1f1726d4cbd23482f8552c80adf2db
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-27 20:09:48 +0530

    Transactions table and one to many relations with accounts ad categories tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,7 @@
-import { pgTable, text } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
@@ -8,8 +10,6 @@ export const accounts = pgTable("accounts", {
   userId: text("user_id").notNull(),
 });
 
-export const insertAccountsSchema = createInsertSchema(accounts);
-
 export const categories = pgTable("categories", {
   id: text("id").primaryKey(),
   plaidId: text("plaid_id"),
@@ -17,4 +17,43 @@ export const categories = pgTable("categories", {
   userId: text("user_id").notNull(),
 });
 
+export const transactions = pgTable("transactions", {
+  id: text("id"),
+  amount: integer("amount").notNull(),
+  payee: text("payee").notNull(),
+  notes: text("notes"),
+  date: timestamp("date", { mode: "date" }).notNull(),
+  accountsId: text("accounts_id")
+    .references(() => accounts.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  categoriesId: text("categories_id").references(() => categories.id, {
+    onDelete: "set null",
+  }),
+});
+
+export const accountsRelations = relations(accounts, ({ many }) => ({
+  transactions: many(transactions),
+}));
+
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  transactions: many(transactions),
+}));
+
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+  accounts: one(accounts, {
+    fields: [transactions.accountsId],
+    references: [accounts.id],
+  }),
+  categories: one(categories, {
+    fields: [transactions.categoriesId],
+    references: [categories.id],
+  }),
+}));
+
+export const insertAccountsSchema = createInsertSchema(accounts);
 export const insertCategoriesSchema = createInsertSchema(categories);
+export const insertTransactionSchema = createInsertSchema(transactions, {
+  date: z.coerce.date(),
+});
