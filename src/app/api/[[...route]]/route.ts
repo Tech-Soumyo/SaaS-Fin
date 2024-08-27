359575716a2e70e958b3b5a543e3f8158156b4a8
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-27 20:10:25 +0530

    transactions route

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -7,6 +7,7 @@ import { handle } from "hono/vercel";
 
 import accounts from "./accounts";
 import categories from "./categories";
+import transactions from "./transactions";
 // import { HTTPException } from "hono/http-exception";
 export const runtime = "edge";
 
@@ -26,7 +27,8 @@ const app = new Hono().basePath("/api");
 
 const routes = app
   .route("/accounts", accounts)
-  .route("/categories", categories);
+  .route("/categories", categories)
+  .route("/transactions", transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
