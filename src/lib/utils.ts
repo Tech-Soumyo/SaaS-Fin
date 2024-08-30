c329cd38b0174153c571658d79a25927ffbfadee
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-30 12:23:32 +0530

    convertamount

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function convertamountToMiliUnits(amount: number) {
+  return Math.round(amount * 83);
+}
+
+export function convertamountFromMiliUnits(amount: number) {
+  return amount / 83;
 }
