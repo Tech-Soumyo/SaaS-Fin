8e4c92005c51c34d2e36da442e5b2aa4f332d6cb
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-30 12:26:37 +0530

    useCreateTransactions hook

---
+++ src/hooks/features/transactions/use-create-transaction.ts
@@ -0,0 +1,30 @@
+import { toast } from "sonner";
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { client } from "@/lib/hono-RPC";
+
+type ResponseType = InferResponseType<typeof client.api.transactions.$post>;
+type RequestType = InferRequestType<
+  typeof client.api.transactions.$post
+>["json"];
+
+export const useCreateTransactions = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.transactions.$post({ json });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Transactions created");
+      queryClient.invalidateQueries({ queryKey: ["transactions"] });
+    },
+    onError: () => {
+      toast.error("Failed to create Transactions");
+    },
+  });
+
+  return mutation;
+};
