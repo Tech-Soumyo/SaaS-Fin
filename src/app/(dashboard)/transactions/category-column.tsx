feb30740c9a2b58b1ec0d12f0d0ffbc881ac4278
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-02 11:31:18 +0530

    category coloumn for transactions

---
+++ src/app/(dashboard)/transactions/category-column.tsx
@@ -0,0 +1,34 @@
+import { useOpenCategory } from "@/hooks/use-open-category";
+import { TriangleAlert } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useOpenTransaction } from "@/hooks/use-open-transaction";
+
+type Props = {
+  id: string;
+  category: string | null;
+  categoryId: string | null;
+};
+
+export const CategoryColumn = ({ id, category, categoryId }: Props) => {
+  const { onOpen: onOpenCategory } = useOpenCategory();
+  const { onOpen: onOpenTransaction } = useOpenTransaction();
+  const onClick = () => {
+    if (categoryId) {
+      onOpenCategory(categoryId);
+    } else {
+      onOpenTransaction(id);
+    }
+  };
+  return (
+    <div
+      onClick={onClick}
+      className={cn(
+        "flex cursor-pointer items-center hover:underline",
+        !category && "text-rose-500"
+      )}
+    >
+      {!category && <TriangleAlert className="mr-2 size-4 shrink-0" />}
+      {category || "Uncategorized"}
+    </div>
+  );
+};
