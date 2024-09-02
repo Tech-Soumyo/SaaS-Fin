ec1dbfb4fc4542be7dbfc3863e7658c8f560d934
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-02 23:08:55 +0530

    frontend of amount

--- src/hooks/features/transactions/use-get-transactions.hook.ts
+++ src/hooks/features/transactions/use-get-transactions.hook.ts
@@ -1,31 +1,28 @@
 import { client } from "@/lib/hono-RPC";
 import { useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "next/navigation";
+import { convertamountFromMiliUnits } from "@/lib/utils";
 
 export const useGetTransactions = () => {
   const params = useSearchParams();
   const from = params.get("from") || "";
   const to = params.get("to") || "";
   const accountId = params.get("accountId") || "";
-
-  const Query = useQuery({
-    queryKey: ["transactons", { from, to, accountId }],
+  const query = useQuery({
+    queryKey: ["transactions", { from, to, accountId }],
     queryFn: async () => {
       const response = await client.api.transactions.$get({
-        query: {
-          from,
-          to,
-          accountId,
-        },
+        query: { from, to, accountId },
       });
-
       if (!response.ok) {
-        throw new Error("Failed to fetch transactons");
+        throw new Error("Failed to fetch transactions");
       }
-
       const { data } = await response.json();
-      return data;
+      return data.map((transaction) => ({
+        ...transaction,
+        amount: convertamountFromMiliUnits(transaction.amount),
+      }));
     },
   });
-  return Query;
+  return query;
 };
