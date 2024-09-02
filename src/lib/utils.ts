bdc3eb06c91688e9d6527c6328eba5e0eb9b7c4a
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-02 23:09:54 +0530

    backend of amount

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,11 @@ export function convertamountToMiliUnits(amount: number) {
 export function convertamountFromMiliUnits(amount: number) {
   return amount / 83;
 }
+
+export function formatCurrency(value: number) {
+  return Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(value);
+}
