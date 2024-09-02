6a49675c59e50d3493385e83aa866a4fe9413703
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-02 23:07:28 +0530

    modify delete component

--- src/hooks/features/transactions/use-delete-transaction.ts
+++ src/hooks/features/transactions/use-delete-transaction.ts
@@ -1,45 +1,38 @@
 import { toast } from "sonner";
 import { InferResponseType } from "hono";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 import { client } from "@/lib/hono-RPC";
 
+// Type definitions for the response and request
 type ResponseType = InferResponseType<
   (typeof client.api.transactions)[":id"]["$delete"]
 >;
 
 export const useDeleteTransaction = (id?: string) => {
   const queryClient = useQueryClient();
-
   const mutation = useMutation<ResponseType, Error>({
     mutationFn: async () => {
-      if (!id) {
-        throw new Error("ID is required to delete the transaction.");
-      }
-
-      const response = await client.api.transactions[":id"]["$delete"]({
+      const response = await client.api.transactions[":id"].$delete({
         param: { id },
       });
-
-      const result = await response.json();
-
-      if (!response.ok) {
-        const error = (result as { error: string }).error || "Unknown error";
-        throw new Error(error);
-      }
-
-      return result as { data: { id: string } };
+      return await response.json();
+    },
+    onMutate: () => {
+      // Display a toast notification when the mutation starts
+      toast.loading("Deleting transaction...");
     },
     onSuccess: () => {
-      toast.success("Account Deleted");
-      queryClient.invalidateQueries({ queryKey: ["transaction", { id }] });
+      // Remove toast.loading
+      toast.dismiss();
+      toast.success("Transaction Deleted");
+      queryClient.invalidateQueries({ queryKey: ["transactions", { id }] });
       queryClient.invalidateQueries({ queryKey: ["transactions"] });
-      //   TODO: Invalidate Summary
+      queryClient.invalidateQueries({ queryKey: ["summary"] });
     },
-    onError: (error) => {
-      console.error("Failed to delete transaction:", error.message);
-      toast.error(`Failed to Delete transaction: ${error.message}`);
+    onError: () => {
+      toast.error("Failed to Delete Transaction");
     },
   });
-
   return mutation;
 };
