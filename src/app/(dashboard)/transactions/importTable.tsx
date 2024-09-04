44b85e756112969c203dcf5742cad23c0b8d334b
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-05 03:39:01 +0530

    importTable

---
+++ src/app/(dashboard)/transactions/importTable.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import TableHeadSelect from "@/app/(dashboard)/transactions/table-head-select";
+
+type Props = {
+  headers: string[];
+  body: string[][];
+  selectedColumns: Record<string, string | null>;
+  onTableHeadSelectChange: (columnIndex: number, value: string | null) => void;
+};
+
+const ImportTable = ({
+  headers,
+  body,
+  selectedColumns,
+  onTableHeadSelectChange,
+}: Props) => {
+  return (
+    <div className="overflow-hidden rounded-md border shadow-sm">
+      <Table>
+        <TableHeader className="bg-muted">
+          <TableRow>
+            {headers.map((_item, index) => (
+              <TableHead key={index}>
+                <TableHeadSelect
+                  columnIndex={index}
+                  selectedColumns={selectedColumns}
+                  onChange={onTableHeadSelectChange}
+                />
+              </TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {body.map((row: string[], index) => (
+            <TableRow key={index}>
+              {row.map((cell, index) => (
+                <TableCell key={index}>{cell}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default ImportTable;
