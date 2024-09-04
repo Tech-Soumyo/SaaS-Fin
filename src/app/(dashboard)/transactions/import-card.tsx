9eb41e7ff6772fec75d931d07b17996f0205ec13
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-05 03:38:50 +0530

    import card imp -> formattedData, progress, onTableHeadSelectChange

---
+++ src/app/(dashboard)/transactions/import-card.tsx
@@ -0,0 +1,135 @@
+// Date input that needs to appear on CSV file
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import ImportTable from "./importTable";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { convertamountToMiliUnits } from "@/lib/utils";
+import { format, parse } from "date-fns";
+import { isValid } from "date-fns";
+const dateFormat = "yyyy-MM-dd";
+const outputFormat = "yyyy-MM-dd";
+
+const requiredOptions = ["amount", "date", "payee"];
+
+interface SelectedColumnsState {
+  [key: string]: string | null;
+}
+
+type Props = {
+  data: string[][];
+  onCancel: () => void;
+  onSubmit: (data: any) => void;
+};
+
+export const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
+  const [selectedColumns, setSelectedColumns] = useState<SelectedColumnsState>(
+    {}
+  );
+
+  const headers = data[0];
+  const body = data.slice(1);
+
+  const onTableHeadSelectChange = (
+    columnIndex: number,
+    value: string | null
+  ) => {
+    setSelectedColumns((prev) => {
+      const newSelectedColumns = { ...prev };
+      for (const key in newSelectedColumns) {
+        if (newSelectedColumns[key] === value) {
+          newSelectedColumns[key] = null;
+        }
+      }
+      if (value === "skip") {
+        value = null;
+      }
+      newSelectedColumns[`column_${columnIndex}`] = value;
+      return newSelectedColumns;
+    });
+  };
+
+  const progress = Object.values(selectedColumns).filter(Boolean).length;
+  const handleContinue = () => {
+    const getColumnIndex = (column: string) => {
+      return column.split("_")[1];
+    };
+
+    const mappedData = {
+      headers: headers.map((_header, index) => {
+        const columnIndex = getColumnIndex(`column_${index}`);
+        return selectedColumns[`column_${columnIndex}`] || null;
+      }),
+      body: body
+        .map((row) => {
+          const transformedRow = row.map((cell, index) => {
+            const columnIndex = getColumnIndex(`column_${index}`);
+            return selectedColumns[`column_${columnIndex}`] ? cell : null;
+          });
+          return transformedRow.every((item) => item === null)
+            ? []
+            : transformedRow;
+        })
+        .filter((row) => row.length > 0),
+    };
+    console.log({ mappedData });
+    const arrayOfData = mappedData.body.map((row) => {
+      return row.reduce((acc: any, cell, index) => {
+        const header = mappedData.headers[index];
+        if (header !== null) {
+          acc[header] = cell;
+        }
+        return acc;
+      }, {});
+    });
+    console.log({ arrayOfData });
+    const formattedData = arrayOfData.map((item) => {
+      const parsedDate = parse(item.date, dateFormat, new Date());
+      const formattedDate = isValid(parsedDate)
+        ? format(parsedDate, outputFormat)
+        : "Invalid Date"; // Fallback value
+
+      return {
+        ...item,
+        amount: convertamountToMiliUnits(parseFloat(item.amount)),
+        date: formattedDate,
+      };
+    });
+
+    console.log({ formattedData });
+    onSubmit(formattedData);
+  };
+  return (
+    <div className="mx-auto -mt-24 w-full max-w-screen-2xl pb-10">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="line-clamp-1 text-xl">
+            Import Transaction
+          </CardTitle>
+          <div className="flex flex-col items-center gap-2 lg:flex-row">
+            <Button size="sm" onClick={onCancel} className="w-full lg:w-auto">
+              Cancel
+            </Button>
+            <Button
+              size="sm"
+              disabled={progress < requiredOptions.length}
+              onClick={handleContinue}
+              className="w-full lg:w-auto"
+            >
+              Continue ({progress} / {requiredOptions.length})
+            </Button>
+          </div>
+        </CardHeader>
+
+        {/* Import Data Table */}
+        <CardContent>
+          <ImportTable
+            headers={headers}
+            body={body}
+            selectedColumns={selectedColumns}
+            onTableHeadSelectChange={onTableHeadSelectChange}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
