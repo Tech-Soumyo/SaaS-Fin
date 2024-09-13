9925e6675dee4d2be97b996d02267fd579430627
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-13 18:41:08 +0530

    Validate Summary

--- src/hooks/features/accounts/use-delete-account.ts
+++ src/hooks/features/accounts/use-delete-account.ts
@@ -34,6 +34,7 @@ export const useDeleteAccount = (id?: string) => {
       queryClient.invalidateQueries({ queryKey: ["account", { id }] });
       queryClient.invalidateQueries({ queryKey: ["accounts"] });
       queryClient.invalidateQueries({ queryKey: ["transactions"] });
+      queryClient.invalidateQueries({ queryKey: ["summary"] });
     },
     onError: (error) => {
       console.error("Failed to delete account:", error.message);
