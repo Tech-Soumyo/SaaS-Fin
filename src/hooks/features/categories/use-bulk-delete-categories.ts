0d8ad4ab27789973266baf269fd28bd084a55f06
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-13 18:41:22 +0530

    Validate Summary

--- src/hooks/features/categories/use-bulk-delete-categories.ts
+++ src/hooks/features/categories/use-bulk-delete-categories.ts
@@ -24,6 +24,7 @@ export const useBulkDeleteCategories = () => {
     onSuccess: () => {
       toast.success("Category Deleted");
       queryClient.invalidateQueries({ queryKey: ["categories"] });
+      queryClient.invalidateQueries({ queryKey: ["summary"] });
       //   TODO: Also Validate Summary
     },
     onError: () => {
