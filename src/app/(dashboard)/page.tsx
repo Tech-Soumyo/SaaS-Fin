45ce3da28a915ef469af8c77d445c21568485282
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-13 19:34:06 +0530

    Dashboard Design with DataGrid and DataCharts

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,24 +1,13 @@
 "use client";
-// import { useGetAccounts } from "@/features-hooks/accounts/api/use-get-accounts.hook";
-import { Button } from "@/components/ui/button";
-import { useNewAccount } from "../../hooks/features/accounts/use-new-account";
-import React from "react";
 
-export default function Home() {
-  const { onOpen } = useNewAccount();
-  // const { data: accounts, isLoading } = useGetAccounts();
-  // if (isLoading) {
-  //   return <div>Loading...</div>;
-  // }
+import { DataCharts } from "@/components/custom/dataCharts";
+import { DataGrid } from "@/components/custom/dataGrid";
+
+export default function DashboardPage() {
   return (
-    <>
-      <Button onClick={onOpen}>Add An Account</Button>
-      {/* <div>
-        {accounts?.map((account) => (
-          <div key={account.id}>{account.name}</div>
-        ))}
-      </div> */}
-      <div>Dashboard</div>
-    </>
+    <div className="mx-auto -mt-24 w-full max-w-screen-2xl pb-10">
+      <DataGrid />
+      <DataCharts />
+    </div>
   );
 }
